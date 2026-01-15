a25499105e60339113dc12368a5f02b87bc422e0
drisanan <drisan@athletenarrative.com>
2026-01-14 22:08:05 -0700

    Fix: Import DateClickArg from @fullcalendar/interaction

--- features/meetings/MeetingsPanel.tsx
+++ features/meetings/MeetingsPanel.tsx
@@ -46,7 +46,8 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import type { EventClickArg, DateClickArg } from '@fullcalendar/core';
+import type { EventClickArg } from '@fullcalendar/core';
+import type { DateClickArg } from '@fullcalendar/interaction';
 
 type Props = {
   clientId: string;
